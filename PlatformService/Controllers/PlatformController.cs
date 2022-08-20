63840fd7477bcd5c21ecd8a6327fc7035ba9fd1d
fady <gamilfady605@gmail.com>
2022-08-20 23:40:50 +0200

    rest of controller actions

--- PlatformService/Controllers/PlatformController.cs
+++ PlatformService/Controllers/PlatformController.cs
@@ -24,7 +24,31 @@ namespace PlatformService.Controller
             this._mapper.Map<IEnumerable<ReadPlatformDto>>(platforms)
          );
       }
-      
 
+      [HttpPost("")]
+      public async Task<IActionResult> CreatePlatform([FromBody] CreatePlatformDto platformDto)
+      {
+         if (platformDto == null){
+            return BadRequest();
+         }
+         var platform = this._mapper.Map<Platform>(platformDto);
+         var creationResult = await this._platformRepo.CreatePlatform(platform);
+         if (creationResult == true){
+            return Ok("Created Successfully");
+         }else{
+            ModelState.AddModelError("", "Error while saving the new platform into the DB");
+            return StatusCode(500, ModelState);
+         }
+      }
+      [HttpGet("{ID:int}")]
+      public async Task<IActionResult> GetPlatformById([FromRoute] int ID)
+      {
+         var platform = await this._platformRepo.GetPlatformById(ID);
+         if (platform == null){
+            return NotFound();
+         }else{
+            return Ok(this._mapper.Map<ReadPlatformDto>(platform));
+         }
+      }
    }
 }
